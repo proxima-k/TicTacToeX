9682c3007f5bd3dec10698f0b3edd9bec9868285
Kent Chua <leonnkent@gmail.com>
2024-08-06 14:51:41 +0200

    feat: player movement and camera rotation

--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -18,10 +18,11 @@ public class PlayerNetwork : NetworkBehaviour
         if (!IsOwner) return;
 
         Vector3 moveDir = Vector3.zero;
-        if (Input.GetKey(KeyCode.W)) moveDir.z = 1;
-        if (Input.GetKey(KeyCode.S)) moveDir.z = -1;
-        if (Input.GetKey(KeyCode.A)) moveDir.x = -1;
-        if (Input.GetKey(KeyCode.D)) moveDir.x = 1;
+        if (Input.GetKey(KeyCode.W)) moveDir += transform.forward;
+        if (Input.GetKey(KeyCode.S)) moveDir -= transform.forward;
+        if (Input.GetKey(KeyCode.A)) moveDir -= transform.right;
+        if (Input.GetKey(KeyCode.D)) moveDir += transform.right;
+        
         
         transform.position += moveDir.normalized * Time.deltaTime * 5f;
         
